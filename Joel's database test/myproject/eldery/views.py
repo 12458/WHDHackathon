06674cfa870b2557b4bb698ecd2cb40aacffb31c
KtYuuki <65496223+KtYuuki@users.noreply.github.com>
2020-05-31 14:03:39 +0800

    updates 2pm

--- Joel's database test/myproject/eldery/views.py
+++ Joel's database test/myproject/eldery/views.py
@@ -1,22 +1,25 @@
 from flask import Blueprint,render_template, url_for, redirect, request, session
 from myproject import db
 from myproject.models import Elderly,Grocery
-from myproject.eldery.forms import FL1,FC2
 
+from flask_bcrypt import Bcrypt
+from hashlib import sha256
+bcrypt = Bcrypt()
 elderly_blueprints = Blueprint('elderly',__name__, template_folder='templates/elderly')
 
 @elderly_blueprints.route('/L1',methods=['GET','POST'])
 def elderly_sign_up():
     if request.method == "POST":
         full_name = request.form["full_name"]
-        email = request.form["email"]
+        username = request.form["username"]
         password = request.form["pass"]
         pass_verify = request.form["pass_verify"]
         contact1 = request.form["contact1"]
         address = request.form["address"]
         area = request.form["area"]
-        if pass_verify == password:
-            new_elderly = Elderly(full_name,email,password,contact1,address,area)
+        if sha256(pass_verify.encode()).hexdigest() == sha256(password.encode()).hexdigest(): # Check if SHA256 Hashes match 
+            password = bcrypt.generate_password_hash(password=password)
+            new_elderly = Elderly(full_name,username,password,contact1,address,area)
             db.session.add(new_elderly)
             db.session.commit()
             session["user"] = name
@@ -27,28 +30,51 @@ def elderly_sign_up():
     else:
         return render_template("L1.html")
 
+@elderly_blueprints.route('/L4',methods=['GET','POST']) #done #Yuki connect this to HTML
+def elderly_login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["pass"]
+        login_or_sign = request.form["login_or_sign"]
+        if login_or_sign == "sign_up":
+            return redirect(url_for("new_user_type"))
+        elif login_or_sign == "submit":        
+            Bleh = Elderly.query.filter_by(username=username)
+            correct = Bleh.password
+            check = bcrypt.check_password_hash(correct,password)
+            if check:
+                session["user"] = username
+                return redirect(url_for("elderly.groceries")) #THIS ONE TO BE CHANGED IF DEF C2 is changed  
+            else:
+                return f"<h1>Invalid credentials</h1>"    
+        else:
+            return render_template("L4.html")
+    else:
+        return render_template("L4.html")
+
+
 @elderly_blueprints.route('/C2',methods=['GET','POST'])
-def C2():
-    form = FC2()
-
-    if form.validate_on_submit():
+def groceries(): #DO NOT CHANGE THIS FUNCTION NAME WITHOUT CHANGING URL ABOVE
+    if "user" in session:
         purchases = form.purchases.data
-        Elderlies_id = form.Elderlies_id.data
-
+        Elderlies_id = form.Elderlies_id.data # need help here - call when going through - Joel - Take out elderlies.id
         grocer = Grocery(purchases,Elderlies_id)
         db.session.add(grocer)
         db.session.commit()
-
-        return redirect(url_for('elderly.C3'))
-    return render_template('C2.html', form=form)
+        return redirect(url_for('elderly.order_checkout'))
+    else:
+        return redirect(url_for("home"))
 
 @elderly_blueprints.route('/C3')
-def C3():
-    return render_template('C3.html')
+def order_checkout():
+    if "user" in session:
+        bleh = Elderly.query.filter_by(username=session["user"])
+        groceies_ordered = bleh.groceries
+        return render_template('C3.html')
 
-@elderly_blueprints.route('/L4')
-def L4():
-    return render_template('L4.html')
-
+@elderly_blueprints.route('/C4')
+def C4():
+    order_status = 'on the way'
+    return render_template('C4.html') #Done as C4 already hardcoded 
 
 
